87a8fd8a35ea8d5c1d509703b87408442f1a4865
Christina Goefron <cagoefron@gmail.com>
2016-04-27 16:31:23 -0400

    Updated PlayerMover script, added more blocks in the scene, started inspectScript and objects.

---
+++ AdventureGame/Assets/inspectScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inspectScript : MonoBehaviour {
+
+	void OnCollisionStay2D(Collision2D other){
+		if (Input.GetButton ("Fire1") && other.gameObject.CompareTag("Player")) {
+			//insert tick canvas text popup on & off
+
+		}
+	}
+}
