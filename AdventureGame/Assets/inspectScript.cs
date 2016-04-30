af03d85ba80b31561012ba8450b171bfec64dbaf
Christina Goefron <cagoefron@gmail.com>
2016-04-30 11:15:43 -0400

    Adjusted Inspect script, added Relic prefab.

--- AdventureGame/Assets/inspectScript.cs
+++ AdventureGame/Assets/inspectScript.cs
@@ -1,12 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class inspectScript : MonoBehaviour {
 
-	void OnCollisionStay2D(Collision2D other){
-		if (Input.GetButton ("Fire1") && other.gameObject.CompareTag("Player")) {
-			//insert tick canvas text popup on & off
+	public GameObject relicCanvas;
+	private bool nearRelic;
+	private bool canvasOn;
 
+	void Update(){
+		
+		if (Input.GetButtonDown ("Fire1") && nearRelic == true) { //ADD LOGIC FOR INSIDE COLLIDER
+			Debug.Log ("Player has hit Z" + nearRelic);
+			//insert tick canvas text popup on & off
+			canvasOn = !canvasOn;
+			relicCanvas.SetActive(canvasOn);
+		} 
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.CompareTag("Player")) {
+			nearRelic = true;
+		}
+	}
+		
+	void OnTriggerExit2D(Collider2D other){
+		if (other.gameObject.CompareTag("Player")) {
+			nearRelic = false;
 		}
 	}
 }
